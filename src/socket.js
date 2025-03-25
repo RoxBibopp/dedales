c94041c13dffff9af8b22247d82830d1c5a7e9c9
RoxBibopp <thibault.malik2@gmail.com>
2025-03-25 18:46:12 +0100

    localhost changed

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 // Changez l'URL si votre serveur n'est pas en localhost
-const socket = io('http://localhost:3000');
+const socket = io('https://dedalback.onrender.com');
 export default socket;
